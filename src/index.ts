d76a2d4d027f06de764b78fb7fa873792014583c
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-12-26 13:25:26 +0800

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import './style.scss'
 
-const duration = new URLSearchParams(location.search).get('duration') ?? '120'
+const searchParams = new URLSearchParams(location.search)
+const color = searchParams.get('color') ?? '0'
+const duration = searchParams.get('duration') ?? '120'
+const size = searchParams.get('size') ?? '71'
 const root = document.querySelector<HTMLElement>(':root')!
 const background = document.querySelector<HTMLElement>('.background')!
 
@@ -8,22 +11,37 @@ class BackgroundController {
   private currentIndex = 0
   private readonly colors = [
     [
-      '#a31e39',
-      '#165B33',
-      '#fff'
-    ],
-    [
-      '#fff',
-      '#a31e39',
-      '#1B9450'
+      '#a31e39', // cell 1
+      '#165B33', // cell 2
+      '#ffffff' // xmas tree
+
     ],
     [
       '#8BE0FF',
-      '#4C9A1E',
-      '#fffff7'
+      '#fffff7',
+      '#1B9450'
+    ],
+    [
+      '#1B9450',
+      '#a31e39',
+      '#ffffff'
+    ],
+    [
+      '#17007c',
+      '#fffff7',
+      '#97cdff'
+    ],
+    [
+      '#F9BABA',
+      '#fffff7',
+      '#D85F5F'
     ]
 
   ]
+
+  changeBackgroundCellSize(size: string) {
+    root.style.setProperty('--size', `${size}px`)
+  }
 
   changeBackgroundDuration(duration: string) {
     root.style.setProperty('--duration', `${duration}s`)
@@ -34,25 +52,28 @@ class BackgroundController {
       color1,
       color2,
       color3
-    ] = this.getColors()
+    ] = this.getColors(Number(color))
     root.style.setProperty('--cell_color1', color1)
     root.style.setProperty('--cell_color2', color2)
     root.style.setProperty('--tree_color', color3)
   }
 
-  getColors() {
-    if (this.colors.length === this.currentIndex) {
+  getColors(index?: number) {
+    if (this.colors.length - 1 === this.currentIndex) {
       this.currentIndex = 0
     } else {
       this.currentIndex++
     }
 
-    return this.colors[this.currentIndex]
+    return this.colors[index ?? this.currentIndex]
   }
 }
 
 const backgroundController = new BackgroundController()
 backgroundController.changeBackgroundDuration(duration)
+backgroundController.changeBackgroundCellSize(size)
+backgroundController.changeBackgroundColors()
+
 background.addEventListener('animationiteration', () => {
   backgroundController.changeBackgroundColors()
 })
